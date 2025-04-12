2f3173da2b06da94fe57c336d1ff223bdd4dcddb
Nathan Cortez <nath.co03@icloud.com>
2025-04-12 18:57:27 -0400

    adds slashes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //we are going to make an EV. It will triger when the DOM is loaded
 
 addEventListener("DOMContentLoaded", async function () {
-  const response = await fetch("https://ten-organized-editor.glitch.me");
+  const response = await fetch("https://beryl-water-dungeon.glitch.me/api/songs");
   const songs = await response.json();
 
   let html = "";
