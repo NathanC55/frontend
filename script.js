a6f830cbbabdefd614861cb37b2103e91bce8e8b
Nathan Cortez <nath.co03@icloud.com>
2025-04-12 14:31:02 -0400

    adds new link

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //we are going to make an EV. It will triger when the DOM is loaded
 
 addEventListener("DOMContentLoaded", async function () {
-  const response = await fetch("https://fabulous-destiny-canid.glitch.me");
+  const response = await fetch("https://ten-organized-editor.glitch.me");
   const songs = await response.json();
 
   let html = "";
